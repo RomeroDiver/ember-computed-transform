0b8c878f3456cb7c6f4251d7d219a41f9c860ae7
Roman Pylypets <diver@DESKTOP-ALPSL3I.localdomain>
2019-06-25 11:18:01 +0200

    Change sourceType to module for parser

--- transformer.js
+++ transformer.js
@@ -4,7 +4,7 @@ const generate = require('@babel/generator').default;
 const t = require("@babel/types");
 
 module.exports = function transform(code) {
-    const ast = parser.parse(code);
+    const ast = parser.parse(code, { sourceType: 'module' });
     traverse(ast, {
         MemberExpression(path) {
             if (path.node.property.name === 'property') {
